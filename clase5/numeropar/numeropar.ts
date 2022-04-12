4b359f20f0698a0fc3de15a048fecbeba48e9c19
Ermangc <94400187+Ermangc@users.noreply.github.com>
2022-04-12 09:28:33 -0300

    dato

--- clase5/numeropar/numeropar.ts
+++ clase5/numeropar/numeropar.ts
@@ -1,2 +1,12 @@
 let btnEnviar = <HTMLButtonElement>document.getElementById("btnEnviar");
 let dato = <HTMLInputElement>document.getElementById("dato");
+let numero: number;
+
+btnEnviar.addEventListener("click", () => {
+  numero = Number(dato.value);
+  if (numero % 2 === 0) {
+    console.log("el numero es par" + numero);
+  } else {
+    console.log("el numero es impar");
+  }
+});
