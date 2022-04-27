632c08cf74ddce597ec70ec7b28a43aee4a92399
Ermangc <94400187+Ermangc@users.noreply.github.com>
2022-04-27 14:46:26 -0300

    clase 4-3

--- clase4/aumento_sueldo/aumento_sueldo.ts
+++ clase4/aumento_sueldo/aumento_sueldo.ts
@@ -1,22 +1,26 @@
 let actualySalary = <HTMLInputElement>document.getElementById("salary");
-actualySalary = actualySalary.value;
 let newSalary: number;
 let btnSend = <HTMLButtonElement>document.getElementById("btnSend");
 
 btnSend.addEventListener("click", () => {
+  actualySalary = actualySalary.value;
   if (0 < actualySalary && actualySalary <= 15000) {
     newSalary = actualySalary * 1.2;
-    console.log("Usted tuvo un aumento del 20%");
-    console.log("Su nuevo sueldo es: $" + newSalary);
+    console.log(
+      "Usted tuvo un aumento del 20%, Su nuevo sueldo es: $" + newSalary
+    );
   } else if (15001 < actualySalary && actualySalary <= 20000) {
     newSalary = actualySalary * 1.1;
-    console.log("Usted tuvo un aumento del 10%");
-    console.log("Su nuevo sueldo es: $" + newSalary);
+    console.log(
+      "Usted tuvo un aumento del 10%, Su nuevo sueldo es: $" + newSalary
+    );
   } else if (20001 < actualySalary && actualySalary <= 25000) {
     newSalary = actualySalary * 1.05;
-    console.log("Usted tuvo un aumento del 5%");
-    console.log("Su nuevo sueldo es: $" + newSalary);
+    console.log(
+      "Usted tuvo un aumento del 5%, Su nuevo sueldo es: $" + newSalary
+    );
   } else if (25000 < actualySalary) {
+    newSalary = actualySalary;
     console.log("Usted no tiene aumento");
   }
 });
