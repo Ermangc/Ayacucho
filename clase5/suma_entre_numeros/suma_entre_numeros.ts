e024f54279db2ec6b62acde82ce70c1f3b82b4b0
Ermangc <94400187+Ermangc@users.noreply.github.com>
2022-04-28 10:40:00 -0300

    suma entre numeros

--- clase5/suma_entre_numeros/suma_entre_numeros.ts
+++ clase5/suma_entre_numeros/suma_entre_numeros.ts
@@ -4,11 +4,20 @@ let current, addiction: number;
 let btnSend = <HTMLButtonElement>document.getElementById("btnSend");
 current = firstNumber;
 addiction = 0;
-btnSend.addEventListener("click", () => {
+/*btnSend.addEventListener("click", () => {
   firstNumber = number1.value;
   secondNumber = number2.value;
   for (current = firstNumber; current <= secondNumber; current++) {
     addiction = addiction + current;
+  }
+  console.log("el resultado de la suma es: ", addiction);
+});*/
+btnSend.addEventListener("click", () => {
+  firstNumber = number1.value;
+  secondNumber = number2.value;
+  while (current <= secondNumber) {
+    addiction = addiction + current;
+    current++;
   }
   console.log("el resultado de la suma es: ", addiction);
 });
