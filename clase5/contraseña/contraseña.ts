2651d5c2565238604bd9af4bb515ba3bff504e22
Ermangc <94400187+Ermangc@users.noreply.github.com>
2022-04-27 23:35:32 -0300

    contraseña

--- clase5/contraseña/contraseña.ts
+++ clase5/contraseña/contraseña.ts
@@ -1,14 +1,18 @@
 const password = "eureka";
-let numberAttemps = 1;
-let passwordEntered : string | null ;
-let correctPassword : boolean = false;
+let numberAttemps = 3;
+let passwordEntered: string | null;
+let correctPassword: boolean = false;
 
 while (correctPassword === false && numberAttemps > 0) {
-  passwordEntered = prompt(`introduzca la contraseña. Le quedan ${numberAttemps} intentos`);
-  if (passwordEntered === password){
+  passwordEntered = prompt(
+    `introduzca la contraseña. Le quedan ${numberAttemps} intentos`
+  );
+  if (passwordEntered === password) {
     correctPassword = true;
     console.log("contraseña correcta");
   }
-  attemps--;
+  numberAttemps--;
 }
-if(numberAttemps <= 0)
+if (numberAttemps <= 0) {
+  console.log("agotaste los intentos");
+}
