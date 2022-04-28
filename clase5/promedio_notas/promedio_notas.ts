00ec67d2ea94162a9105b2db4544aa64dcd90c7a
Ermangc <94400187+Ermangc@users.noreply.github.com>
2022-04-28 18:06:11 -0300

    promedio

--- clase5/promedio_notas/promedio_notas.ts
+++ clase5/promedio_notas/promedio_notas.ts
@@ -1,5 +1,20 @@
-let suma, nota, promedio, contador: number;
-contador = 1;
-suma = 0;
+let nombreAlumno = <HTMLInputElement> document.getElementById("nombreAlumno");
+let notaPractica, notaTeoria, notaFinal, notaProblema;
+let btnEnviar = <HTMLButtonElement> document.getElementById("btnEnviar");
 
-while(contador<=10)
+btnEnviar.addEventListener("click", () => {
+  nombreAlumno = nombreAlumno.value;
+  notaPractica = notaPractica.value;
+  notaTeoria = notaTeoria.value;
+  notaProblema = notaProblema.value;
+  while(nombreAlumno <> "" || notaPractica <> "" || notaTeoria <> "" || notaProblema <> "") {
+    if(notaPractica <=10 && notaPractica >= 0) && (notaTeoria <=10 && notaTeoria >= 0) && (notaProblema <=10 && notaProblema >=0) {
+       notaFinal = notaPractica*0.1 + notaProblema*0.5 + notaTeoria*0.4;
+       console,log("La nota final de ", nombreAlumno, "es: " + notaFinal)
+    }else {
+      console.log("Error notas ingresadas");
+    }
+    let nombreAlumno = prompt("Ingrese nombre del alumno");
+  }
+})
+
