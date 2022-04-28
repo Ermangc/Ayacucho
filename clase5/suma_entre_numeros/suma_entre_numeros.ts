e514528342367475b5002bc9c9093b22f49cf0db
Ermangc <94400187+Ermangc@users.noreply.github.com>
2022-04-28 10:11:51 -0300

    calse5

--- clase5/suma_entre_numeros/suma_entre_numeros.ts
+++ clase5/suma_entre_numeros/suma_entre_numeros.ts
@@ -1,3 +1,14 @@
-let firstNumber: number = number1.value;
-let secondNumber: number = number2.value;
+let firstNumber: number;
+let secondNumber: number;
 let current, addiction: number;
+let btnSend = <HTMLButtonElement>document.getElementById("btnSend");
+current = firstNumber;
+addiction = 0;
+btnSend.addEventListener("click", () => {
+  firstNumber = number1.value;
+  secondNumber = number2.value;
+  for (current = firstNumber; current <= secondNumber; current++) {
+    addiction = addiction + current;
+  }
+  console.log("el resultado de la suma es: ", addiction);
+});
