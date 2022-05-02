fc9eda07310219e0eece303e1450e0449a93a4d4
Ermangc <94400187+Ermangc@users.noreply.github.com>
2022-05-02 18:48:53 -0300

    final

--- clase5/tabla_multiplicacion/tabla_multiplicacion.ts
+++ clase5/tabla_multiplicacion/tabla_multiplicacion.ts
@@ -1,11 +1,13 @@
 let numTabla = <HTMLInputElement>document.getElementById("date1");
 let limTabla = <HTMLInputElement>document.getElementById("date2");
+let btnEnviar = <HTMLButtonElement>document.getElementById("btnEnviar");
 
-numTabla = Number(date1.value);
-limTabla = Number(date2.value);
 let resultado: number = 0;
-
-for (let contador: number = 1; contador <= limTabla; contador++) {
-  resultado = numTabla * contador;
-  console.log(numTabla, "x", contador, "=", resultado);
-}
+btnEnviar.addEventListener("click", () => {
+  numTabla = Number(date1.value);
+  limTabla = Number(date2.value);
+  for (let contador: number = 1; contador <= limTabla; contador++) {
+    resultado = numTabla * contador;
+    console.log(numTabla, "x", contador, "=", resultado);
+  }
+});
