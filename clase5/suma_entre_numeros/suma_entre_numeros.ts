dd1ad0efbe4b4c801d1af7efb109eafa2521bbe9
Ermangc <94400187+Ermangc@users.noreply.github.com>
2022-05-02 19:36:58 -0300

    final

--- clase5/suma_entre_numeros/suma_entre_numeros.ts
+++ clase5/suma_entre_numeros/suma_entre_numeros.ts
@@ -1,24 +1,24 @@
-let firstNumber: number = <HTMLInputElement>document.getElementById("number1");
-let secondNumber: number = <HTMLInputElement>document.getElementById("number2");
+let firstNumber = <HTMLInputElement>document.getElementById("number1");
+let secondNumber = <HTMLInputElement>document.getElementById("number2");
 let current, addiction: number;
 let btnSend = <HTMLButtonElement>document.getElementById("btnSend");
 current = firstNumber;
 addiction = 0;
+
 btnSend.addEventListener("click", () => {
   firstNumber = Number(number1.value);
   secondNumber = Number(number2.value);
-  while ((current <= secondNumber){
+  for (current = firstNumber; current <= secondNumber; current++) {
     addiction = addiction + current;
-    current++;
   }
   console.log("el resultado de la suma es: ", addiction);
 });
-/*btnSend.addEventListener("click", () => {
-  firstNumber = number1.value;
-  secondNumber = number2.value;
-  for (current = firstNumber; current <= secondNumber; current++) {
+
+/*firstNumber = number1.value;
+secondNumber = number2.value;
+while (current <= secondNumber) {
   addiction = addiction + current;
+  current++;
 }
 console.log("el resultado de la suma es: ", addiction);
-});
-  */
+*/
