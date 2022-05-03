43025c28ac400c89c39e40eb4bd2a5418a619377
Ermangc <94400187+Ermangc@users.noreply.github.com>
2022-05-03 14:04:20 -0300

    final

--- clase5/dados/dados.ts
+++ clase5/dados/dados.ts
@@ -5,10 +5,10 @@ let btnEnviar = <HTMLButtonElement>document.getElementById("btnEnviar");
 let probabCara: number = 1 / 6;
 let probabFinal: number = 1;
 
-btnEnviar.addEventListener("click", () => ){
+btnEnviar.addEventListener("click", () => {
   n = Number(dato.value);
   for (let contador = 1; contador <= n; contador++) {
     probabFinal = probabFinal * probabCara;
   }
     console.log("la Probabilidad es ", probabFinal);
-}
+)}
